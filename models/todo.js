08a02edfb135a2af64cd0c625510da32bbedcd44
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-03-27 21:04:42 +0800

    add todo and user model

--- models/todo.js
+++ models/todo.js
@@ -1,24 +1,11 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Todo.init({
+  const Todo = sequelize.define('Todo', {
     name: DataTypes.STRING,
     isDone: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
-  return Todo;
-};
+  }, {})
+  Todo.associate = function (models) {
+    // associations can be defined here
+  }
+  return Todo
+}
