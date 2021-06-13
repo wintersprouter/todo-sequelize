5d3070201e66fd971f42dde3ee3966f84b325b62
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-06-13 16:27:49 +0800

    feat: add createTodo feature

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,10 +2,27 @@ const db = require('../models')
 const { Todo } = db
 
 const todoController = {
+  getNewTodoPage: (req, res) => {
+    return res.render('new')
+  },
+  createTodo: async (req, res) => {
+    try {
+      const UserId = req.user.id
+      const name = req.body.name
+      if (!name) {
+        return res.redirect('back')
+      }
+      await Todo.create({ name, UserId })
+      return res.redirect('/')
+    } catch (err) {
+      console.log(err)
+    }
+  },
   getTodoPage: async (req, res) => {
     try {
+      const UserId = req.user.id
       const id = req.params.id
-      const todo = await Todo.findByPk(id)
+      const todo = await Todo.findOne({ where: { id, UserId } })
       res.render('detail', { todo: todo.toJSON() })
     } catch (err) {
       console.log(err)
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,5 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:id', todoController.getTodoPage)
+router.get('/new', todoController.getNewTodoPage)
+router.post('/', todoController.createTodo)
 
 module.exports = router
